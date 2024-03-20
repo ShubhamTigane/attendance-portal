eb7b942e76d96bfa4fc08dc9a6a62ed66eba0c5c
ShubhamTigane <131232793+ShubhamTigane@users.noreply.github.com>
2024-03-20 16:50:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(cors())
+app.use(cors(
+  {origin:[" "],
+methods:["POST","GET"],
+credentials:true
+}
+))
 app.use(express.json());
 app.use(morgan("dev"));
 
